0f077e05de61270af269accf9947f6b736e47a8b
mikaelec <mikaelec@gmail.com>
2016-05-12 09:13:24 +0200

    Updated calc_usage and check_password (non-labelled) examples.

--- calc_usage.c
+++ calc_usage.c
@@ -1,25 +1,39 @@
 typedef struct usage {
   int start_time;
-  int usageWh;
+  int usage_in_Wh;
 } usage;
 
 typedef struct price {
   int start_time;
-  int priceEars;
+  int price_in_cents;
 } price;
 
-int calc_usage(usage *usages, int usages_count, price *prices, int prices_count) {
+usage* get_latest_usage();
+price* get_latest_prices();
+void send_to_consumer(int bill);
+void send_to_electrical_company(int bill);
+
+int calculate_bill() {
+  int usage_count = 100;
+  int prices_count = 100;
+  usage *latest_usage = get_latest_usage();
+  price *latest_prices = get_latest_prices();
   int result = 0;
 
   int i = 0;
   int j = 0;
-  while (i < usages_count) {
-    while ((j < prices_count-1) && (prices[j+1].start_time <= usages[i].start_time)) {
+  while (i < usage_count) {
+    while ((j < prices_count-1) && (latest_prices[j+1].start_time <= latest_usage[i].start_time)) {
       j = j + 1;
     }
-      result = result + usages[i].usageWh * prices[j].priceEars;
-      i = i + 1;
+    result = result + latest_usage[i].usage_in_Wh * latest_prices[j].price_in_cents;
+    i = i + 1;
   }
-
   return result;
+}
+
+int main(int argc, char **argv) {
+  int bill = calculate_bill();
+  send_to_consumer(bill);
+  send_to_electrical_company(bill);
 }
--- check_password.c
+++ check_password.c
@@ -1,20 +1,31 @@
 #include <stdbool.h>
 #include <string.h>
 
-typedef struct pinfo {
-  char name[20];
-  char pass[20];
-} pinfo;
+typedef struct user_info {
+  char username[20];
+  char password[20];
+} user_info;
 
-bool check_password(pinfo *db, int db_size, char *username, char *password) {
+user_info* get_users();
+void send_response(bool is_match);
+
+bool check_password(char *username, char *password) {
+  int user_count = 100;
+  user_info *users = get_users();
   int i = 0;
   bool match = false;
 
-  while (i < db_size) {
-    if (!strcmp(db[i].name, username) && !strcmp(db[i].pass, password)) {
+  while (i < user_count) {
+    if (!strcmp(users[i].username, username) && !strcmp(users[i].password, password)) {
       match = true;
     }
     i = i + 1;
   }
+
   return match;
+}
+
+int main(int argc, char **argv) {
+  bool is_match = check_password(argv[1], argv[2]);
+  send_response(is_match);
 }
